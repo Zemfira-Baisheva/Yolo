2696bf61b0bb9ed63795033425f48be4f6716971
Zemfira <zemfira.baisheva@gmail.com>
2024-11-22 17:59:12 +0500

    6

--- Info.py
+++ Info.py
@@ -146,5 +146,39 @@ st.image(conf_mat_s, caption=' ', use_container_width=True)
 
 
 
+st.write("### Семантическая сегментация аэрокосмических снимков")
+
+st.markdown("#### Число эпох - ")
+st.markdown("#### Объем выборок:")
+st.markdown("1. тренировочная выборка - 10 200 изображений, 2. валидационная -  изображений")
+
+st.subheader("Метрики модели")
+
+conf_mat_face = Image.open('images_metrics/confusion_matrix_face.png')
+results_face = Image.open('images_metrics/results_face.png')
+pr_curve_face = Image.open('images_metrics/PR_curve_face.png')
+p_curve_face = Image.open('images_metrics/P_curve_face.png')
+r_curve_face = Image.open('images_metrics/R_curve_face.png')
+f1_curve_face = Image.open('images_metrics/F1_curve_face.png')
+
+st.markdown("### Графики Loss-функции")
+st.image(results_face, caption=' ', use_container_width=True)
+
+st.markdown("### Precision-recall Кривая")
+st.image(pr_curve_face, caption=' ', use_container_width=True)
+
+st.markdown("### F1-Кривая")
+st.image(f1_curve_face, caption=' ', use_container_width=True)
+
+st.markdown("### Precision Кривая")
+st.image(p_curve_face, caption=' ', use_container_width=True)
+
+st.markdown("### Recall Кривая")
+st.image(r_curve_face, caption=' ', use_container_width=True)
+
+st.markdown("### Матрица ошибок")
+st.image(conf_mat_face, caption=' ', use_container_width=True)
+
+
 
 
